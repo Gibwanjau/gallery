bc53ffdd68b05ee7208add85415d2520135d98cf
Gibwanjau <107396492+Gibwanjau@users.noreply.github.com>
2025-03-30 14:22:21 +0300

    mongo user update

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://gibwanjau0:<db_password>@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://gibwanjau0:<db_password>@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    test: 'mongodb+srv://gibwanjau0:<db_password>@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    production: 'mongodb+srv://gallery:Gallery12345@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://gallery:Gallery12345@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://gallery:Gallery12345@cluster0.dxryi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
